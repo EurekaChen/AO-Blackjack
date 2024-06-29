03e02f3ef1d765b8e353db3b97fb865b5b7e248d
Eureka Chen <eureka@ee-studio.com>
2024-06-29 10:28:17 +0800

    实现语言选择功能

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { t, locales, locale } from '$lib';
+</script>
+<div>
+    <span style="font-size: 24px;" role="img" aria-label="选择语言">🌐</span>
+<select bind:value={$locale}>
+	{#each $locales as value}
+		<option {value}>{$t(`lang.${value}`)}</option>
+	{/each}
+</select>
+</div>
+<slot />
+
+
