72deed86e4e4f5fb96d06f42733214217a2ef206
Eureka Chen <eureka@ee-studio.com>
2024-07-01 20:06:45 +0800

    实现筹码堆叠效果

--- src/lib/component/PlayerStack.svelte
+++ src/lib/component/PlayerStack.svelte
@@ -3,20 +3,11 @@
 
 </script>
 
-<div style="text-align:center;width:50px;color:#64b5f6;height:20px; position:absolute;left:278px;top:350px;">
-	<Stack amount="600"/>
+<div style="position:absolute;left:120px;top:360px;">
+	<Stack amount={4995}/>
 </div>
 
-<div>
-	<img
-		id="dealerStack"
-		src="/img/chip/5000.png"
-		alt="5"
-		style="width:46px;position:absolute;left:477px;top:310px;"
-	/>
-	<div
-		style="text-align:center;width:50px;color:#64b5f6;height:20px; position:absolute;left:478px;top:350px;"
-	>
-		7000
-	</div>
+<div style="position:absolute;left:477px;top:340px;">
+	<Stack amount={155}/>
 </div>
+
--- src/lib/component/Stack.svelte
+++ src/lib/component/Stack.svelte
@@ -1,36 +1,56 @@
 <script lang="ts">
 	export let amount;
-	
+
 	//设计思路：从大到小算出每种筹码的个数
 	//从大到小列筹码
 	//let chipArray = [10000000, 2500000, 500000, 100000, 50000, 10000, 2500, 500, 100, 50, 25, 10, 5, 1];
 	//我当前筹码是这些：
-	let chipArray = [25000, 5000, 1000, 500, 100, 25, 10, 5];
-	let chipCountArray = [0, 0, 0, 0, 0, 0, 0, 0];
+	let chipArray = [25000, 5000, 1000, 500, 100, 25, 5];
+	let chipCountArray = [0, 0, 0, 0, 0, 0, 0];
 
+	let amt = amount;
 	for (let index = 0; index < chipArray.length; index++) {
 		let chip = chipArray[index];
-		//大筹码个数
-		chipCountArray[index] = Math.floor(amount / chip);
-		amount -= chipCountArray[index] * chipArray[index];
-	}
+		//筹码个数
+		chipCountArray[index] = Math.floor(amt / chip);
+		amt -= chipCountArray[index] * chipArray[index];
+	}	
 
-	let chipIndex = 0;
+	let chipImgArr = [];
+	let position = 0;
+	for (let i = 0; i < 7; i++) {
+		for (let j = 1; j <= chipCountArray[i]; j++) {
+			position += 1;
+			chipImgArr.push({ img: chipArray[i], position: position });
+		}
+	}
 </script>
 
-<div>
-	{#each chipArray as chip,i}
-		{i}
-        {chip}
-		{#each Array(chipCountArray[chipIndex]).fill() as _, j}
-			<img
-				src="/img/chip/{chip}.png"
-				alt={chip.toString()}
-				style="width:46px;position:absolute;"
-			/>
-		{/each}
+<div style="position:relative;width:50px;">
+	{#each chipImgArr as chipImg}
+		<img
+			class="chip"
+			src="/img/chip/{chipImg.img}.png"
+			alt={chipImg.img.toString()}
+			style="top:{-30 - chipImg.position * 6}px;"
+		/>
 	{/each}
-	<div style="color:#64b5f6;height:20px; position:absolute;">
+	<div class="amount">
 		{amount}
 	</div>
 </div>
+
+<style>
+	.chip {
+		position: absolute;
+		width: 45px;
+		left: 0px;
+	}
+	.amount {
+		text-align: center;
+		width: 50px;
+		color: #64b5f6;
+		height: 20px;
+		position: absolute;
+	}
+</style>
