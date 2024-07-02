73eb9ec88f161500f1198ae3161d1e3464c17077
Eureka Chen <eureka@ee-studio.com>
2024-07-02 14:35:29 +0800

    制作”清除“图标

--- src/lib/component/Action.svelte
+++ src/lib/component/Action.svelte
@@ -4,18 +4,24 @@
 </script>
 <style>
 	.text{
-		margin-top:-10px;border:1px solid green;text-align:center;font-weight: bolder;
+		margin-top:-4px;text-align:center;font-weight: bolder;
 	}
 </style>
 
-<div style="position:absolute;left:580px;top:480px;">
+<div id="double" style="position:absolute;left:580px;top:480px;">
 	<object data="/img/action/double.svg" title="hit" style="width:60;height:60;" />
- 	<div class="text" style="color:#ff9800">加倍</div>
+ 	<div class="text" style="color:#ff9800">加 倍</div>
 </div>
 
 
-<div style="position:absolute;left:680px;top:455px;">
+<div id="deal" style="position:absolute;left:680px;top:458px;">
 	<object data="/img/action/hit.svg" title="hit" style="width:60;height:60;fill:green" />
- 	<div style="border:1px solid green;text-align:center;color:white">停牌</div>
+ 	<div class="text" style="color:#90caf9">发 牌</div>
 </div>
+
+<div style="position:absolute;left:780px;top:426px;">
+	<object data="/img/action/clear.svg" title="hit" style="width:60;height:60;fill:green" />
+ 	<div class="text" style="color:#cccccc">清 除</div>
+</div>
+
 
