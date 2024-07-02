3288d70350555638dd705a5130c7fcd5b4b303d4
Eureka Chen <eureka@ee-studio.com>
2024-07-02 09:01:27 +0800

    制作了一个发牌按钮svg

--- src/lib/component/Action.svelte
+++ src/lib/component/Action.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Game } from '$lib/store/Game';
+
 </script>
 
 <div style="position:absolute;left:480px;top:485px;">
@@ -10,9 +11,7 @@
 </div>
 
 <div style="position:absolute;left:680px;top:455px;">
-	<svg width="60" height="60" >
-		<circle cx="30" cy="30" r="25" fill="none" stroke="#90caf9" stroke-width="2" />		
-	</svg>	
-	<div style="border:1px solid green;text-align:center;color:white">要牌</div>
+	<object data="/img/action/hit.svg" title="hit" style="width:60;height:60;fill:green" />
+ 	<div style="border:1px solid green;text-align:center;color:white">停牌</div>
 </div>
 
