3b28c2428f9100e35f5b6c20584fe7e130182fd5
Eureka Chen <eureka@ee-studio.com>
2024-07-05 10:17:36 +0800

    筹码区点击没反应了！

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,7 @@
-<script lang="ts">	
-	import ChipSelector from '$lib/component/ChipSelector.svelte'
-	import ActionPanel from '$lib/component/ActionPanel.svelte'
-	import PlayerStack from '$lib/component/ChipStack.svelte'
-	import GameHand from '$lib/component/GameHand.svelte';
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	onMount(() => {
+		goto('/play');
+	});
 </script>
-	
-<div style="width:1024px;height:576px;position:fixed">
-	<PlayerStack />
-	<ChipSelector />
-	<ActionPanel />
-	<GameHand />
-
-</div>
---
+++ src/routes/play/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">	
+	import ChipSelector from '$lib/component/ChipSelector.svelte'
+	import ActionPanel from '$lib/component/ActionPanel.svelte'
+	import PlayerStack from '$lib/component/ChipStack.svelte'
+	import GameHand from '$lib/component/GameHand.svelte';
+</script>
+	
+<div style="width:1024px;height:576px;position:fixed">
+	<PlayerStack />
+	<ChipSelector />
+	<ActionPanel />
+	<GameHand />
+
+</div>
