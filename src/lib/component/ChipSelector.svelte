14fa0e1b2fcc602bf80eca258a984b53e23d9310
Eureka Chen <eureka@ee-studio.com>
2024-07-08 16:14:25 +0800

    <a>改用<button>

--- src/lib/component/ChipSelector.svelte
+++ src/lib/component/ChipSelector.svelte
@@ -64,61 +64,67 @@
 		}
 	}
 </script>
+<style>
+	button{
+		background: none;
+		border: none;
+	}
+</style>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div id="chipSelector" class:disabled on:click|capture={handleClick}>
-	<a href="./#" on:click={downChip}>
+	<button on:click={downChip}>
 		<img
 			id="down"
 			src="/img/chip/left-arrow.png"
 			alt="down"
 			style="width:30px;position:absolute;left:-2px;top:357px;cursor:pointer;display:none"
 		/>
-	</a>
+	</button>
 
-	<a href="./#" on:click={() => betChip($ChipRank[chipOffset])}>
+	<button on:click={() => betChip($ChipRank[chipOffset])}>
 		<img
 			id="chip0"
 			src="/img/chip/{$ChipRank[chipOffset]}.png"
 			alt="5"
 			style="width:50px;position:absolute;left:20px;top:367px;cursor:pointer;"
 		/>
-	</a>
+	</button>
 
-	<a href="./#" on:click={() => betChip($ChipRank[chipOffset + 1])}>
+	<button on:click={() => betChip($ChipRank[chipOffset + 1])}>
 		<img
 			id="chip1"
 			src="/img/chip/{$ChipRank[chipOffset + 1]}.png"
 			alt="25"
 			style="width:50px;position:absolute;left:80px;top:395px;cursor:pointer;"
 		/>
-	</a>
+	</button>
 
-	<a href="./#" on:click={() => betChip($ChipRank[chipOffset + 2])}>
+	<button on:click={() => betChip($ChipRank[chipOffset + 2])}>
 		<img
 			id="chip2"
 			src="/img/chip/{$ChipRank[chipOffset + 2]}.png"
 			alt="100"
 			style="width:50px;position:absolute;left:138px;top:420px;cursor:pointer;"
 		/>
-	</a>
+	</button>
 
-	<a href="./#" on:click={() => betChip($ChipRank[chipOffset + 3])}>
+	<button on:click={() => betChip($ChipRank[chipOffset + 3])}>
 		<img
 			id="chip3"
 			src="/img/chip/{$ChipRank[chipOffset + 3]}.png"
 			alt="500"
 			style="width:50px;position:absolute;left:198px;top:443px;cursor:pointe;"
 		/>
-	</a>
+	</button>
 
-	<a href="./#" on:click={upChip}>
+	<button on:click={upChip}>
 		<img
 			id="up"
 			src="/img/chip/right-arrow.png"
 			alt="right arrow"
 			style="width:30px;position:absolute;left:255px;top:465px;cursor:pointer;"
 		/>
-	</a>
+	</button>
 </div>
