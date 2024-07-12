42069d71a98eadc4a0a6cac677dde4921eaae7e4
Eureka Chen <eureka@ee-studio.com>
2024-07-13 07:49:49 +0800

    删除restore原先的代码

--- src/routes/play/+layout.svelte
+++ src/routes/play/+layout.svelte
@@ -98,77 +98,16 @@
 	}
 
 	//还原游戏
-	function restore(aoPlayer:AoPlayer ) {
-		//console.log('aoPlayer.state:', aoPlayer.state);	
-		//$Player.state.activeHandIndex = aoPlayer.state.activeHandIndex - 1;	
-
-		// for (let card of aoPlayer.state.hands[0].cards) {
-		// 	$Player.state.hands[0].cards.push(card);
-		// }
-		//$Player.state.hands[0].amount = aoPlayer.state.hands[0].amount;
-
-		// if (aoPlayer.state.hands.length > 1) {
-		// 	//有两手：
-		// 	//恢复第二手
-		// 	for (let card of aoPlayer.state.hands[1].cards) {
-		// 		$Player.state.hands[1].cards.push(card);
-		// 	}
-		// 	$Player.state.hands[1].amount = aoPlayer.state.hands[1].amount;
-
-			
-		// 	if(aoPlayer.state.activeHandIndex==0){
-		// 		//两手21，牌局结束：
-		// 		$Player.inGame=false;
-		// 		$Player.state.originalAmount=aoPlayer.state.originalAmount;
-		// 		$Action.newHand=true;
-		// 	}
-		// 	else if(aoPlayer.state.activeHandIndex==1){
-		// 		//第一手活动：			
-		// 	}
-		// } else {
-		// 	//一手时判断是否可拆牌
-		// 	let rank1 = aoPlayer.state.hands[0].cards[0].charAt(0);
-		// 	let rank2 = aoPlayer.state.hands[0].cards[1].charAt(0);
-		// 	if (rank1 == 'T' || rank1 == 'J' || rank1 == 'Q' || rank1 == 'K') rank1 = '10';
-		// 	if (rank2 == 'T' || rank2 == 'J' || rank2 == 'Q' || rank2 == 'K') rank2 = '10';
-		// 	if (rank1 == rank2) {
-		// 		$Action.split = true;
-		// 	}
-
-		// 	if (aoPlayer.state.dealerCards[0].charAt(0) == 'A') {
-		// 		//一手时可下保险
-		// 		if (aoPlayer.state.insurance > 0) {
-		// 			//已经下保险
-		// 			$Player.state.insurance = aoPlayer.state.insurance;
-		// 		} else {
-		// 			$Action.insurance = true;
-		// 		}
-		// 	}
-		// }
-
-		//$Player.state.dealerCards = aoPlayer.state.dealerCards;
-		// for (let card of state.dealerCards) {
-		// 	$Player.state.dealerCards.push(card);
-		// }
-		//$Player=$Player;
-		// const hand1CanDouble=aoPlayer.state.activeHandIndex==1 && aoPlayer.state.hands[0].cards.length==2;
-		// const hand2CanDouble=aoPlayer.state.activeHandIndex==2 && aoPlayer.state.hands[1].cards.length==2;
-		
-		// if(hand1CanDouble || hand2CanDouble){
-		// 	Action.afterDeal(true);
-		// }
-		// else{
-		// 	Action.afterDeal(false);
-		// }		
-		
+	function restore(aoPlayer:AoPlayer ) {	
 		Player.getState(aoPlayer);
-
 		if(aoPlayer.state.activeHandIndex==2){
 			//玩第二手牌
-			if(aoPlayer.state.hands[1].cards.length==2){
+			if(aoPlayer.state.hands[1].cards.length==2){				
+				//可加倍
 				Action.afterDeal(true)
 			}
 			else{
+				//不可加倍
 				Action.afterDeal(false)
 			}			
 		}
@@ -180,19 +119,18 @@
 			}
 
 			if(aoPlayer.state.hands[0].cards.length==2){
-				
-				Action.afterDeal(true)								
+				//可加倍
+				Action.afterDeal(true)	
+				//可拆牌							
 				if(isSamePoint(aoPlayer.state.hands[0].cards)){
 					$Action.split=true;
 				}
 			}			
 			else{
+				//不可加倍
 				Action.afterDeal(false)
-			}			
-
+			}
 		}	
-
-
 	}
 
 	onMount(async () => {
