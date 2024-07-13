b229f3b0aff480b7380b0a9baef3ded68bcac5bc
Eureka Chen <eureka@ee-studio.com>
2024-07-13 20:54:30 +0800

    修正直接Action改状不影响UI的错误

--- src/lib/state/hit.ts
+++ src/lib/state/hit.ts
@@ -1,25 +1,28 @@
-import { Action } from "$lib/store/Action";
+import { Action } from '$lib/store/Action';
 
-import { Player } from "$lib/store/Player";
-import type { AoPlayer } from "$lib/type";
+import { Player } from '$lib/store/Player';
+import type { AoPlayer } from '$lib/type';
 
 import { get } from 'svelte/store';
-import { showResult } from "./showResult";
-import { log } from "$lib/store/Debug";
+import { showResult } from './showResult';
+import { log } from '$lib/store/Debug';
 
+export function hit(aoPlayer: AoPlayer) {
+	log('要牌后aoPlayer:', aoPlayer);
+	if (aoPlayer.state.dealerCards.length > 1) {
+		//给庄家发牌，说明牌局结束了。
+		showResult(aoPlayer);
+		Player.getState(aoPlayer);
 
-export function hit(aoPlayer:AoPlayer) {
-    log('要牌后aoPlayer:', aoPlayer);
-    if (aoPlayer.state.dealerCards.length > 1) {
-        //给庄家发牌，说明牌局结束了。       
-        showResult(aoPlayer);
-        Player.getState(aoPlayer);
-        get(Action).newHand = true;
-        log("newHand:",get(Action).newHand);       
-        get(Player).inGame = false;
-    } else {
-        Player.getState(aoPlayer);
-        Action.afterDeal(false);
-    }
+		//get(Action).newHand = true;
+		const currentAction = get(Action);
+		currentAction.newHand = true;
+		Action.set(currentAction);
+		log('newHand:', get(Action).newHand);
+
+		get(Player).inGame = false;
+	} else {
+		Player.getState(aoPlayer);
+		Action.afterDeal(false);
+	}
 }
-
