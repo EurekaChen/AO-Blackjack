36ef8030c6ad1bbc60ce5933b2e0658689e2ccdf
Eureka Chen <eureka@ee-studio.com>
2024-07-15 10:24:42 +0800

    删除ChipSelector中的key好象不影响筹码更新

--- src/lib/component/ChipStack.svelte
+++ src/lib/component/ChipStack.svelte
@@ -3,20 +3,18 @@
 	import { Player } from '$lib/store/Player';
 </script>
 
-{#key $Player.state}
-	<div style="position:absolute;left:120px;top:360px;">
-		<Stack amount={$Player.balance} />
-	</div>
+<div style="position:absolute;left:120px;top:360px;">
+	<Stack amount={$Player.balance} />
+</div>
 
-	<div style="position:absolute;left:477px;top:340px;">
-		<Stack amount={$Player.state.hands[0].amount} />
-	</div>
+<div style="position:absolute;left:477px;top:340px;">
+	<Stack amount={$Player.state.hands[0].amount} />
+</div>
 
-	<div style="position:absolute;left:677px;top:280px;">
-		<Stack amount={$Player.state.hands[1].amount} />
-	</div>
+<div style="position:absolute;left:677px;top:280px;">
+	<Stack amount={$Player.state.hands[1].amount} />
+</div>
 
-	<div style="position:absolute;left:310px;top:170px;">
-		<Stack amount={$Player.state.insurance} />
-	</div>
-{/key}
+<div style="position:absolute;left:310px;top:170px;">
+	<Stack amount={$Player.state.insurance} />
+</div>
