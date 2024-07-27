a57c5ef71bc2d5f53db738fbb274b0e372fc0bb7
Eureka Chen <eureka@ee-studio.com>
2024-07-27 21:02:10 +0800

    给EGC余额情况进行分页

--- src/routes/admin/egc/+page.svelte
+++ src/routes/admin/egc/+page.svelte
@@ -2,48 +2,76 @@
 	import { egcProcess } from '$lib';
 	import { createDataItemSigner } from '@permaweb/aoconnect';
 	import { message, result } from '$lib/store/Setting';
-	import {Waiting } from '$lib/store/Waiting';
+	import { Waiting } from '$lib/store/Waiting';
+	import { LightPaginationNav, paginate } from 'svelte-paginate';
+	import { log } from '$lib/store/Debug';
 
-	let egcTable;
-	async function GetPlayers() {
+	let currentPage = 1;
+	let pageSize = 10;
+	let items = [];
+	$: paginatedItems = paginateAsync(currentPage);
 
-		$Waiting.isWaiting=true;
-		$Waiting.waitingText="从AO获取所有地址EGC余额信息";
+	async function refresh() {
+		items = await GetEgcBalaces();
+		paginatedItems = paginateAsync(currentPage);
+	}
+
+	async function paginateAsync(currentPage: number) {
+		let pageItems = paginate({ items, pageSize, currentPage });
+		return pageItems;
+	}
+
+	async function GetEgcBalaces() {
+		$Waiting.isWaiting = true;
+		$Waiting.waitingText = '从AO获取所有地址EGC余额信息';
 		let msgId = await message({
 			process: egcProcess,
 			tags: [{ name: 'Action', value: 'Balances' }],
 			signer: createDataItemSigner(window.arweaveWallet)
 		});
-		
+
 		console.log(msgId);
-		$Waiting.waitingText="解析AO数据";
+		$Waiting.waitingText = '解析AO数据';
 		let r = await result({ message: msgId, process: egcProcess });
 		console.log(r);
 		let data = r.Messages[0].Data;
 		console.log(data);
-		egcTable = JSON.parse(data);		
-		console.log(egcTable);		
-		$Waiting.isWaiting=false;
+		let balances = JSON.parse(data);
+		log('余额数据', balances);
+		$Waiting.isWaiting = false;
+
+		let entries = Object.entries(balances);
+
+		return entries;
 	}
 </script>
 
-
-<button class="btn btn-primary" on:click={GetPlayers}>点击获取所有玩家信息</button>
-{#if egcTable}
-<table class="table table-bordered table-striped">
-    <thead>
-        <tr>              
-            <th>钱包地址</th>
-			<th>EGC数量</th>			
-        </tr>
-    </thead>
-    <tbody>
-        {#each  Object.entries(egcTable) as [key, value]}
-            <tr>
-                <td>{key}</td>                             
-				<td>{value}</td>				
-            </tr>
-        {/each}
-    </tbody>
-</table>
-{/if}
+<button class="btn btn-primary" on:click={refresh}>点击获取所有EGC地址信息</button>
+{#await paginatedItems then pitems}
+	<table class="table table-bordered table-striped">
+		<thead>
+			<tr>
+				<th>钱包地址</th>
+				<th>EGC数量</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each pitems as addr}
+				<tr>
+					<td>{addr[0]}</td>
+					<td>{addr[1] / 100}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+	<LightPaginationNav
+		totalItems={items.length}
+		{pageSize}
+		{currentPage}
+		limit={1}
+		showStepOptions={true}
+		on:setPage={(e) => {
+			currentPage = e.detail.page;
+		}}
+	/>
+{/await}
